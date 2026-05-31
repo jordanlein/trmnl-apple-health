4a9fd797ddc3bff1df66e84cf25bafb5a2f45475
jordanlein <jordanleinberger@gmail.com>
2026-05-31 11:22:24 -0500

    Fix dark mode tab bar icons

--- ios/TRMNLHealthSync/TRMNLHealthSync/TRMNLHealthSyncApp.swift
+++ ios/TRMNLHealthSync/TRMNLHealthSync/TRMNLHealthSyncApp.swift
@@ -1,9 +1,31 @@
 import SwiftUI
+import UIKit
 
 @main
 struct TRMNLHealthSyncApp: App {
     @StateObject private var model = AppModel()
     @Environment(\.scenePhase) private var scenePhase
+
+    init() {
+        let selectedColor = UIColor { traits in
+            traits.userInterfaceStyle == .dark ? .white : .black
+        }
+        let appearance = UITabBarAppearance()
+        appearance.configureWithDefaultBackground()
+        [
+            appearance.stackedLayoutAppearance,
+            appearance.inlineLayoutAppearance,
+            appearance.compactInlineLayoutAppearance,
+        ].forEach { itemAppearance in
+            itemAppearance.selected.iconColor = selectedColor
+            itemAppearance.selected.titleTextAttributes = [
+                .foregroundColor: selectedColor,
+            ]
+        }
+
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
 
     var body: some Scene {
         WindowGroup {
